298d4e9e9ecfa0661541ded584b32ee24e6191ab
LogutovIlja <99278246+LogutovIlja@users.noreply.github.com>
2022-04-29 18:09:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,14 +4,13 @@
 
 
 double pown(double value, uint16_t n) {
-  using namespace std;
-    cout << pow(value,n);
-    return 0;
+  double i;
+    i = pow(value,n);
+    return i;
 }
 
 uint64_t fact(uint16_t n) {
-  using namespace std;
-    uint64_t k = n , j = n ;
+  uint64_t k = n , j = n;
     for (uint64_t i = 1; i <= j - 1; i++) {
         k = k * i;
     }
@@ -19,19 +18,13 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  using namespace std;
-    double k;
-    cout.setf(ios::fixed);
-    cout.precision(19);
+  double k;
     k = x / fact(n);
     return k;
 }
 
 double expn(double x, uint16_t count) {
-  using namespace std;
-    double k = 1;
-    cout.setf(ios::fixed);
-    cout.precision(16);
+  double k = 1;
     for (uint16_t i = 1; i <= count; i++) {
         k = k + (pow(x, i) / fact(i));
     }
@@ -39,11 +32,8 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  using namespace std;
-    double k = 0;
+  double k = 0;
     uint16_t i , j;
-    cout.setf(ios::fixed);
-    cout.precision(16);
     for (uint16_t n = 1; n <= count; n++) {
         i = (2 * n) - 1;
         j = n - 1;
@@ -53,15 +43,13 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-  using namespace std;
-    double k = 1;
+  double k = 1;
     uint16_t i, j;
-    cout.setf(ios::fixed);
-    cout.precision(17);
     for (uint16_t n = 2; n <= count; n++) {
         i = (2 * n) - 2;
         j = n - 1;
         k = k + (pow(-1, j) * (pow(x, i) / fact(i)));
+    
     }
     return k;
 }
